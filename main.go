0fc234450dc6f94e96d4606cb128a4b1c9cab5b7
l00kAhead <himanshusoni0925@gmail.com>
2023-07-23 17:18:32 +0700

    refactored

--- main.go
+++ main.go
@@ -9,19 +9,37 @@ import (
 )
 
 type WordFreq struct {
-	word  string
-	count int
+	word string
+	freq int
+}
+
+func (p WordFreq) String() string {
+	return fmt.Sprintf("%s %d", p.word, p.freq)
 }
 
 func main() {
-	//read text from file
-	bs, err := ioutil.ReadFile("file.txt")
+	filePath := "project\\word_count\\gen_ai.txt" 
+	text, err := readFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	text := string(bs)
 
-	//split the text into words
+	words := countWords(text)
+	wordFreqs := convertToWordFreqs(words)
+	sortWordFreqs(wordFreqs)
+
+	printTopWords(wordFreqs, 10)
+}
+
+func readFile(filePath string) (string, error) {
+	bs, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(bs), nil
+}
+
+func countWords(text string) map[string]int {
 	reg := regexp.MustCompile("[a-zA-Z']+")
 	matches := reg.FindAllString(text, -1)
 
@@ -29,16 +47,25 @@ func main() {
 	for _, match := range matches {
 		words[match]++
 	}
+	return words
+}
 
+func convertToWordFreqs(words map[string]int) []WordFreq {
 	var wordFreqs []WordFreq
 	for k, v := range words {
 		wordFreqs = append(wordFreqs, WordFreq{k, v})
 	}
+	return wordFreqs
+}
 
-	sort.Slice(wordFreqs, func(i, j int) bool { return wordFreqs[i].count > wordFreqs[j].count })
+func sortWordFreqs(wordFreqs []WordFreq) {
+	sort.Slice(wordFreqs, func(i, j int) bool {
+		return wordFreqs[i].freq > wordFreqs[j].freq
+	})
+}
 
-	for i := 0; i < 10 && i < len(wordFreqs); i++ {
+func printTopWords(wordFreqs []WordFreq, n int) {
+	for i := 0; i < n && i < len(wordFreqs); i++ {
 		fmt.Println(wordFreqs[i])
 	}
-
 }
